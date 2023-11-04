eaf7043b38477cf61d67465be759d4fb792a4bc4
CyberRex <hspwinx86@gmail.com>
2023-11-04 23:40:12 +0900

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,118 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first scheduled worker.
+ *
+ * - Run `wrangler dev --local` in your terminal to start a development server
+ * - Run `curl "http://localhost:8787/cdn-cgi/mf/scheduled"` to trigger the scheduled event
+ * - Go back to the console to see what your worker has logged
+ * - Update the Cron trigger in wrangler.toml (see https://developers.cloudflare.com/workers/wrangler/configuration/#triggers)
+ * - Run `wrangler deploy --name my-worker` to deploy your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/runtime-apis/scheduled-event/
+ */
+
+// https://qiita.com/YOS0602/items/8eadf8f7743ebdc5946c からコピー
+export const format = (str: string, ...args: unknown[]): string => {
+	for (const [i, arg] of args.entries()) {
+	  const regExp = new RegExp(`\\{${i}\\}`, 'g')
+	  str = str.replace(regExp, arg as string)
+	}
+	return str
+}
+
+const postTemplate = '昨日のMisskeyの活動は\n\n\
+ノート: {0}({1})\n\
+フォロー: {2}({3})\n\
+フォロワー: {4}({5})\n\
+\n\
+でした。\n\
+<small>Powered by Cloudflare Workers</small>\
+';
+
+export interface Env {
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	// MY_KV_NAMESPACE: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+	
+	KV: KVNamespace;
+	MISSKEY_HOST: string;
+	MISSKEY_TOKEN: string;
+	POST_VISIBILITY: 'public' | 'home' | 'followers' | 'specified' | undefined;
+}
+
+export interface UserProfile {
+	id: string;
+	followersCount: number;
+	followingCount: number;
+	notesCount: number;
+}
+
+export interface KVUserCountStore {
+	followersCount: number;
+	followingCount: number;
+	notesCount: number;
+}
+
+export default {
+	async scheduled(
+		controller: ScheduledController,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<void> {
+		// Fetch my profile data
+		const profreq = await fetch(`https://${env.MISSKEY_HOST}/api/i`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ i: env.MISSKEY_TOKEN }),
+		});
+		if (!profreq.ok || profreq.status != 200) {
+			throw new Error('Failed to fetch profile');
+		}
+
+		const prof: UserProfile = await profreq.json();
+		const KVS_KEY = `${prof.id}_count`;
+
+		// Get previous day's count from KV
+		const kvs: KVUserCountStore | null = await env.KV.get(KVS_KEY, 'json');
+		if (kvs !== null) {
+			
+			// calculate delta and post
+			const notesCountDelta = prof.notesCount - kvs.notesCount;
+			const followingCountDelta = prof.followingCount - kvs.followingCount;
+			const followersCountDelta = prof.followersCount - kvs.followersCount;
+
+			const text = format(postTemplate,
+				prof.notesCount,
+				(notesCountDelta >= 0 ? '+' : '-' ) + notesCountDelta,
+				prof.followingCount,
+				(followingCountDelta >= 0 ? '+' : '-' ) + followingCountDelta,
+				prof.followersCount,
+				(followersCountDelta >= 0 ? '+' : '-' ) + followersCountDelta,
+			);
+
+			await fetch(`https://${env.MISSKEY_HOST}/api/notes/create`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					i: env.MISSKEY_TOKEN,
+					text,
+					visibility: env.POST_VISIBILITY,
+				}),
+			});
+		}
+
+		await env.KV.put(KVS_KEY, JSON.stringify({
+			notesCount: prof.notesCount,
+			followingCount: prof.followingCount,
+			followersCount: prof.followersCount,
+		}));
+	},
+};
